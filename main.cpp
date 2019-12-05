5770f65c38d7e8e2e38912a5c433d442cacd7c7a
Ting-Wei-Chen <51171682+Ting-Wei-Chen@users.noreply.github.com>
2019-12-06 01:08:42 +0800

    add basic setting

--- main.cpp
+++ main.cpp
@@ -7,6 +7,31 @@ const int col_number=21;
 
 int wall[row_number][col_number];
 
+int score=0,level=1;
+int x_pos=22,y_pos=10;
+int left_plate=9,right_plate=12;
+/*
+0:nothing
+1:wall
+2:block
+3:plate
+4:bomb
+*/
+void setup(){
+    for(int i=0;i<row_number;++i){
+        for(int j=0;j<col_number;++j){
+            if(i==0||j==0||i==row_number-1||j==col_number-1){
+                wall[i][j]=1;
+            }
+        }
+    }
+
+    wall[23][9]=3;
+    wall[23][10]=3;
+    wall[23][11]=3;
+    wall[23][12]=3;
+}
+
 int main(){
     system("pause");
     return 0;
