822431f4f68a9eec775717c2a31b375513ad6c6e
Ting-Wei-Chen <51171682+Ting-Wei-Chen@users.noreply.github.com>
2019-12-06 00:34:34 +0800

    add size of map

--- main.cpp
+++ main.cpp
@@ -2,6 +2,11 @@
 #include<windows.h>
 #include<conio.h>
 
+const int row_number=28;
+const int col_number=21;
+
+int wall[row_number][col_number];
+
 int main(){
     system("pause");
     return 0;
